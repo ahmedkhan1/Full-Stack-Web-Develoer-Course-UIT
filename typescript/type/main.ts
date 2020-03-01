3e23846054bec642e6bc0f1df684b3182eb87438
Ahmed Khan <ahmed_rises@hotmail.com>
2020-03-01 22:04:52 +0500

    types

---
+++ typescript/type/main.ts
@@ -0,0 +1,19 @@
+let mynumber : any;
+let noType : number;
+let bool : boolean;
+let name : string;
+
+
+let third = [];
+
+let fourth: number[] =  [1,2,3,4,5];
+
+let fifth: string[] =  ["","","""];
+
+let tuple : [number, string] =  [1, "hello"];
+
+enum color {
+    black,  // Gets default values assigned started from 0 and increments on
+    blue
+};
+console.log(color.black); // Here when inserting . after color will show the default values assigned in Intelli sence
